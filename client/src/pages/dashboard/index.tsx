99daaba2e3a8511b1338bec429876cae946884d8
Vic <victor.ekpo@gmail.com>
2024-07-20 21:40:46 -0500

    update css

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,52 +1,36 @@
 import { Theme1Card } from "@/components/Cards/Theme1";
 import { Theme2Card } from "@/components/Cards/Theme2";
+import './dashboard.css';
 
 const top4Genres = new Array(4).fill(null);
-
 const top20Songs = new Array(20).fill(null);
 
 const DashboardPage = () => {
   return (
     <>
-      <h4 style={{ textAlign: "center", marginTop: 20, fontSize: 14 }}>Good day! Browse the most popular genres and
-        songs.</h4>
+      <h4 className="db-h4">
+        Good day! Browse the most popular genres and songs.
+      </h4>
       <br/>
-      <div className='db-top-4-genres'
-           style={{
-             display: "flex",
-             gap: "80px 0",
-             alignItems: "center",
-             justifyContent: "space-evenly",
-             flexWrap: "wrap",
-             marginBottom: 50
-           }}>
-        {top4Genres.map(k => (
-          <div style={{ flex: "0 0 300px" }}>
-            <Theme1Card/>
-          </div>
+      <div className='db-top-4-genres'>
+        {top4Genres.map((k, i) => (
+          <section>
+            <Theme1Card key={i}/>
+          </section>
         ))}
       </div>
 
-      <hr style={{ borderColor: "#162033" }}/>
+      <hr/>
 
-      <div className='db-top-20-songs'
-           style={{
-             display: "flex",
-             gap: "40px 40px",
-             alignItems: "center",
-             justifyContent: "space-evenly",
-             flexWrap: "wrap",
-             marginTop: 50,
-             marginBottom: 50
-           }}>
-        {top20Songs.map(k => (
-          <div style={{ flex: "0 0 150px" }}>
-            <Theme2Card/>
-          </div>
+      <div className='db-top-20-songs'>
+        {top20Songs.map((k, i) => (
+          <section>
+            <Theme2Card key={i}/>
+          </section>
         ))}
       </div>
 
-      <hr style={{ borderColor: "#162033" }}/>
+      <hr/>
 
     </>
   )
