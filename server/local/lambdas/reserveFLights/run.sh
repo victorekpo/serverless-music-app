d98113b22e12e679927b7943bcb132621c6ad19a
Vic <victor.ekpo@gmail.com>
2024-08-11 19:59:56 -0500

    clean up

--- server/local/lambdas/reserveFLights/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-npm run synth
-
-# sam.cmd for windows regular sam for linux/mac
-sam.cmd local invoke graphqlHandler89480F20 \
-  -t ../../../cdk.out/MusicApp-Fns.template.json \
-  --env-vars ./env.json \
-  --event ./event.json
