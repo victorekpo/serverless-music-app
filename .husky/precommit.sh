50f33a4e83cac1985d9e54f1ece3cb888faacce0
Vic <victor.ekpo@gmail.com>
2024-09-08 03:53:47 -0500

    feat: auth

--- .husky/precommit.sh
+++ .husky/precommit.sh
@@ -5,6 +5,7 @@ if [ -f "$FILE" ]; then
   sed -i -e 's/account_id = ".*"/account_id = "**********"/' $FILE
   sed -i -e 's/pattern = ".*"/pattern = "**********"/' $FILE
   sed -i -e 's/API_URL = ".*"/API_URL = "**********"/' $FILE
+  sed -i -e 's/API_KEY = ".*"/API_KEY = "**********"/' $FILE
   echo "$FILE updated"
   git add -A
 else
